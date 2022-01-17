344debc83f156583186e07b8cb80e2164317cc70
venkatasubramanian <vkandavelu@hotmail.com>
2022-01-17 11:51:39 +0530

    My First MavenTestng project commit

---
+++ src/test/java/com/vs/MyFirstTest.java
@@ -0,0 +1,20 @@
+package com.vs;
+
+import org.testng.annotations.Test;
+
+public class MyFirstTest {
+    String msg = System.getProperty("name");
+    String id = System.getProperty("UX");
+   @Test
+    public void test1()
+    {
+
+        System.out.println("Test 1 pass "+msg);
+        System.out.println("Test 1 pass with id"+id);
+    }
+    @Test
+    public void test2()
+    {
+        System.out.println("Test 2 pass "+msg);
+    }
+}
---
+++ src/test/java/com/vs/MySecondTest.java
@@ -0,0 +1,17 @@
+package com.vs;
+
+import org.testng.annotations.Test;
+
+public class MySecondTest {
+
+    @Test
+    public void secondTest1()
+    {
+        System.out.println("Second Test 1 pass");
+    }
+    @Test
+    public void secondTest2()
+    {
+        System.out.println("Second Test 2 pass");
+    }
+}
